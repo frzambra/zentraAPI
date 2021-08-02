9426552c0ee1400333b9cebe61bd8220f0fe7ead
frzambra <frzambra@gmail.com>
2021-08-02 10:53:26 -0400

    add documentation and improve function getDataZentra

--- R/getDataZentra.R
+++ R/getDataZentra.R
@@ -1,7 +1,16 @@
 #' Function to get data from the API of zentracloud.com
 #'
 #' @param token token get from zentracloud API
-#' @param var which variable you want to get
+#' @param var which variable you want to get they could be
+#'  "Air Temperature","Atmospheric Pressure","Battery Percent",     
+#' "Battery Voltage","Gust Speed","Lightning Activity",   
+#' "Lightning Distance","Logger Temperature","Max Precip Rate"      
+#' "Precipitation","RH Sensor Temp","Reference Pressure",  
+#' "Saturation Extract EC","Soil Temperature","Solar Radiation",      
+#' "VPD", "Vapor Pressure", "Water Content",        
+#' "Wind Direction","Wind Speed","X-axis Level"         
+#' "Y-axis Level"         
+#' @param device_sn the name of the z6 datalogger
 #' @param port the port number of sensor
 #' @param time_span vector of two having time range for which the data is required
 #'
@@ -10,18 +19,18 @@
 #' @importFrom jsonlite fromJSON
 #' @importFrom httr GET add_headers content
 #' @importFrom lubridate now days
-#' @importFrom tibble tibble
+#' @importFrom tibble tibble 
 #' @examples
 #' # Get data of VPD for the last five days
 #' # token <- my_token
 #' # VPD_data <- getDataZentra(token,var = "VPD",port =1)
 
-getDataZentra <- function(token,var='Water Content',port,time_span=c(now(),now()-days(5))){
+getDataZentra <- function(token,var='Water Content',device_sn = "z6-10293",port,time_span=c(now(),now()-days(5))){
   url = "https://zentracloud.com/api/v3/get_readings/"
   resp <- GET(url,
               add_headers("Authorization" =token ),
               query = list(
-                device_sn = "z6-10293",
+                device_sn = device_sn,
                 end_date =format(time_span[1],"%m-%d-%Y %H:%M"),
                 start_date = format(time_span[2],"%m-%d-%Y %H:%M")
               ))
@@ -37,10 +46,10 @@ getDataZentra <- function(token,var='Water Content',port,time_span=c(now(),now()
   value <- .parseZENTRA(data,varPos,idPort,"value")
   units <- sapply(data[['data']][[varPos]],'[[',1)[6,idPort]
   
-  tibble(time,value,units$units)
+  out <- tibble(time=time,value=value,units=units$units)
+  return(out)
 }
 
-#' @export
 .parseZENTRA <- function(x,varPos,idPort,reading){
   sapply(x[['data']][[varPos]][[idPort]][[2]],'[[',reading)
 }
--- R/test.R
+++ R/test.R
@@ -1,4 +1,4 @@
-#library(devtools)
+#library(zentraAPI)
 
 token <- "Token e8357468a4eda4fdc97d480fbd0ba0cfac3c0e4b"
-#a <- getDataZentra(token,var = "VPD",port =1)
+#a <- getDataZentra(token,var = "Water Content",port =2)
